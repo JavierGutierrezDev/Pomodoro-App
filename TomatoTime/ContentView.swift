d4bf9c7febed2e868aaf4b0e98ba0adfe2f69d99
JavierGutierrezDev <gutierrezgutierrezjavier30@gmail.com>
2023-09-26 18:45:04 +0200

    Initial Commit

---
+++ TomatoTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TomatoTime
+//
+//  Created by Javier Gutierrez on 26/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TomatoTime/TomatoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoTimeApp.swift
+//  TomatoTime
+//
+//  Created by Javier Gutierrez on 26/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TomatoTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
